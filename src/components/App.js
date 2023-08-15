dbb0780246b26ae03c6e896f7d1aee85f4f43598
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-08-15 22:54:04 +0530

    contact field

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+//import ContactList from "./ContactList";
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Header />
+      <AddContact />
+    </div>
+  );
+}
+export default App;
