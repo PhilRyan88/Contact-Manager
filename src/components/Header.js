00f334395bd4e91464cd4b43ba77e810481c8a22
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-08-27 23:58:45 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
-        <h3>Contact Handler</h3>
+        <h2>Contact Handler</h2>
       </div>
     </div>
   );
